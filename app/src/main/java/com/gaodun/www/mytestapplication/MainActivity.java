fb392b232e4d889b14f3e13d0845c344796a14f2
GuolinYao <635142285@qq.com>
2016-06-02 13:51:21 +0800

    修改跳转

--- app/src/main/java/com/gaodun/www/mytestapplication/MainActivity.java
+++ app/src/main/java/com/gaodun/www/mytestapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gaodun.www.mytestapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.view.LayoutInflaterCompat;
@@ -10,11 +11,20 @@ import android.support.v7.app.AppCompatDelegate;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.tv_main)
+    TextView tvMain;
+    @BindView(R.id.iv_icon)
+    ImageView ivIcon;
     private Typeface typeFace;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
                 AppCompatDelegate delegate = getDelegate();
                 View view = delegate.createView(parent, name, context, attrs);
-                if(view!=null&& (view instanceof TextView)){
+                if (view != null && (view instanceof TextView)) {
                     ((TextView) view).setTypeface(typeFace);
                 }
                 return view;
@@ -36,11 +46,20 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
     }
 
     public void onClick(View v) {
-//        Intent intent = new Intent(this, BActivity.class);
-//        startActivity(intent);
+        switch (v.getId()) {
+            case R.id.iv_icon:
+                intent = new Intent(this, BActivity.class);
+                break;
+            case R.id.tv_main:
+                 intent = new Intent(this, Main2Activity.class);
+                break;
+        }
+
+        startActivity(intent);
     }
 }
